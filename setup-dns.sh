f60cfb000611adafa79a9786eaf212185c2b55d9
DNS <37093+DNS@users.noreply.github.com>
2025-09-30 02:05:57 +0700

    -

--- setup-dns.sh
+++ setup-dns.sh
@@ -4,3 +4,10 @@ cd ~/sandbox
 git clone git@github.com:DNS/BIN.git
 git clone git@github.com:DNS/PC-Neon-Setup.git
 git clone git@github.com:DNS/Debian-Help.git
+
+
+# mouse scroll
+$a = gc -Raw ~/.config/kdeglobals
+$a -Replace '\[KDE\]', "[KDE]`nWheelScrollLines=8" > ~/.config/kdeglobals
+
+
