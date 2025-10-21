0a0ce12d8a05a6ac66d6c01f3b9f2852bd3d98fa
DNS <37093+DNS@users.noreply.github.com>
2025-10-22 00:41:30 +0700

    -

--- setup-root.sh
+++ setup-root.sh
@@ -5,7 +5,6 @@
 passwd -d root
 passwd -d dns
 
-#touch ~/.hushlogin
 touch /root/.hushlogin
 
 ###################################
@@ -14,15 +13,10 @@ apt clean
 apt update
 
 apt install -y ubuntu-advantage-tools xdg-desktop-portal-kde software-properties-common ttf-mscorefonts-installer maliit-keyboard
-
 apt install -y systemd-sysv htop tmux neofetch zenmap whois zfsutils-linux gdisk sshfs
-
 apt install -y micro nano wxhexeditor scite scilab
-
 apt install -y firefox thunderbird libreoffice neofetch nginx qbittorrent qbittorrent-nox krita gimp wget kaffeine vlc retroarch otpclient oathtool radeontop glmark2 piper kcalc artha iw
-
 apt install -y clang gcc g++ binutils gcc-mingw-w64 g++-mingw-w64 make openjdk-17-jdk openjdk-17-jre git nodejs
-
 apt install -y python3-pip python3-pylsp yt-dlp instaloader
 
 
@@ -41,12 +35,10 @@ pwsh
 
 ###################################
 # Change default shell
-@'
-#!/usr/bin/pwsh
+echo '#!/usr/bin/pwsh
 
 /usr/bin/pwsh -Login -NoLogo
-
-'@ | Out-File /usr/bin/pwsh.nologo
+' > /usr/bin/pwsh.nologo
 
 chmod +x /usr/bin/pwsh.nologo
 
@@ -64,16 +56,5 @@ git config --global init.defaultBranch master
 
 
 # wake from USB devices
-grep . /sys/bus/usb/devices/*/power/wakeup
-echo enabled > /sys/bus/usb/devices/usb1/power/wakeup
-echo enabled > /sys/bus/usb/devices/usb2/power/wakeup
-echo enabled > /sys/bus/usb/devices/usb3/power/wakeup
-echo enabled > /sys/bus/usb/devices/usb4/power/wakeup
-echo enabled > /sys/bus/usb/devices/usb5/power/wakeup
-echo enabled > /sys/bus/usb/devices/usb6/power/wakeup
-echo enabled > /sys/bus/usb/devices/usb7/power/wakeup
-echo enabled > /sys/bus/usb/devices/usb8/power/wakeup
-
-
-
-
+echo enabled > grep . /sys/bus/usb/devices/*/power/wakeup
+# gci /sys/bus/usb/devices/*/power/wakeup | %{ 'enabled' > $_ }
